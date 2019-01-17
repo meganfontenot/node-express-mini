70241a02d4cc4613df601c253f7a16a4156c895c
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-01-17 16:39:26 -0600

    Viewing Required datad form db data, Error msgs set.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// implement your API here
-
-//Build server using Express > Returns Function > Function returns Obj > Obj reps the Server
-
+// implement your API here //Build server using Express > Returns Function > Function returns Obj > Obj reps the Server
 const express = require('express');
 
 const greeter = require('./greeter.js');
+const db = require('./data/db');
 
 const server = express();
 
@@ -16,7 +14,31 @@ server.get('/greet', (req, res) => {
     res.json({ hello: 'stranger' });
 });
 
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(user => {
+        if (user) {
+            res.status(200).json(user);
+        }else{
+            res.status(404).json({ message: 'user not found' });
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: "we fail, cant get users", error: err });
+    });
+});
+
 server.get('/greet/:person', greeter);
 
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.findById(id).then(user => {
+        res.status(200).json(user);
+    }).catch(err => {
+        res.status(500).json({ message: "we fail, cant get user", error: err });
+    })
+
+});
 
 server.listen(9000, () => console.log('Keep breathing'));
