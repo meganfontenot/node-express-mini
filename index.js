560d48cddf06ca668881cbbcc15456147c24c8b7
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-01-17 15:28:55 -0600

    Server up: greeter.js: greeting function running in port index.js: Server running with express, returning a greeting on port 9000

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+
+//Build server using Express > Returns Function > Function returns Obj > Obj reps the Server
+
+const express = require('express');
+
+const greeter = require('./greeter.js');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.json('alive');
+});
+
+server.get('/greet', (req, res) => {
+    res.json({ hello: 'stranger' });
+});
+
+server.get('/greet/:person', greeter);
+
+
+server.listen(9000, () => console.log('Keep breathing'));
